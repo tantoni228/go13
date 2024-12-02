75a8df5bf77b8c692c3dd3a6d1f95a97c0d08146
Anton <leshyovantoha@yandex.ru>
2024-12-02 23:25:24 +0300

    generate ogen files

--- generate.go
+++ generate.go
@@ -1,5 +1,5 @@
 package project
 
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target pkg/messages-service --clean api/messages-service.yml &&
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target pkg/users-service --clean api/users-service.yml &&
-//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target pkg/chats-service --clean api/chats-service.yml
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target pkg/ogen/messages-service --clean api/messages-service.yml &&
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target pkg/ogen/users-service --clean api/users-service.yml &&
+//go:generate go run github.com/ogen-go/ogen/cmd/ogen@latest --target pkg/ogen/chats-service --clean api/chats-service.yml
